e259edc409ea227597bccfce20a102789f0c9cd3
abshir24 <abshirm@live.com>
2023-12-12 15:58:04 -0800

    testing logs

--- src/api/PostRequest.js
+++ src/api/PostRequest.js
@@ -3,9 +3,9 @@ import axios from "axios"
 const API = axios.create({baseUrl: process.env.REACT_APP_SERVER_URL})
 
 export const getRemedy = (ailment) =>{
-    console.log(API.baseUrl)
+    console.log("URL", process.env.REACT_APP_SERVER_URL)
     return API.post(`remedy/getremedy`, { ailment: ailment })
-} 
+}
 
 export const getHerb = (herbName) => API.get(`herb/getherb/${herbName}`)
 
