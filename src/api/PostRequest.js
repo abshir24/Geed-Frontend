2cdc89c51fac728a2f83081c9a919543088f1879
abshir24 <abshirm@live.com>
2023-12-13 16:51:40 -0800

    updated postrequest.js

--- src/api/PostRequest.js
+++ src/api/PostRequest.js
@@ -2,11 +2,11 @@ import axios from "axios"
 
 const API = axios.create({baseUrl: process.env.REACT_APP_SERVER_URL})
 
-export const getRemedy = (ailment) => API.post(`remedy/getremedy`, { ailment: ailment })
+export const getRemedy = (ailment) => API.post(`/remedy/getremedy`, { ailment: ailment })
 
 
-export const getHerb = (herbName) => API.get(`herb/getherb/${herbName}`)
+export const getHerb = (herbName) => API.get(`/herb/getherb/${herbName}`)
 
-export const getAllHerbs = () => API.get(`herb/getallherbs`)
+export const getAllHerbs = () => API.get(`/herb/getallherbs`)
 
-export const getAllRemedies = () => API.get(`remedy/getallremedies`)
+export const getAllRemedies = () => API.get(`/remedy/getallremedies`)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect }from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux"
-import { useDispatch } from "react-redux"
 import App from "./App";
 import "./index.css";
 import store from "./store/ReduxStore"
